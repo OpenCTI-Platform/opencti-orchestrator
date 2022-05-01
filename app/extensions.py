b1e6bef853cf7f705d409329b8aa11fe2ea3b28e
nor3th <73965465+nor3th@users.noreply.github.com>
2022-05-01 07:49:46 +0200

    [plugin] Added elasticsearch and broker plugin, remove sqlalchemy and marshmallow

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,7 @@
 from flask_apscheduler import APScheduler
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-db = SQLAlchemy()
-ma = Marshmallow()
+from app.modules.elasticsearch import FlaskElasticsearch
+from app.modules.broker import BrokerHandler
+
 scheduler = APScheduler()
+elastic = FlaskElasticsearch()
+broker = BrokerHandler()
