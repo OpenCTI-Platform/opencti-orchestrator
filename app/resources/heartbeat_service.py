d5fd58466c592d1d07463706de1a09678524855e
nor3th <73965465+nor3th@users.noreply.github.com>
2022-05-01 07:50:23 +0200

    [orchestrator] Added empty heartbeat checking service

---
+++ app/resources/heartbeat_service.py
@@ -0,0 +1,11 @@
+# class HeartbeatService(object):
+#     def __init__(self):
+import Datetime as Datetime
+
+
+def heartbeat_service(app_launch: Datetime):
+    pass
+    # TODO implement heartbeat service
+    # check every run when each connector last contacted the scheduler
+    # and disable connectors which haven't been responding in X time
+    # pass
