85b68ae1fd8d8acb47b8c6102c732ec0bf0addf1
nor3th <73965465+nor3th@users.noreply.github.com>
2022-05-01 07:47:15 +0200

    [plugin] added broker extension

---
+++ app/modules/broker.py
@@ -0,0 +1,48 @@
+from typing import Optional, List
+from app.core.brokers import pika_broker, std_broker
+from flask import _app_ctx_stack
+
+from app.resources.utils import BrokerClass
+
+brokers = {
+    "RABBITMQ": pika_broker.PikaBroker,
+    "STDOUT": std_broker.StdBroker,
+}
+
+
+class BrokerHandler(object):
+    def __init__(self, app=None, **kwargs) -> None:
+        self.app = app
+        if app is not None:
+            self.init_app(app, **kwargs)
+
+    def init_app(self, app, **kwargs) -> None:
+        if self.app is None:
+            self.app = app
+
+        self.args = kwargs
+
+        app.teardown_appcontext(self.teardown)
+
+    def connect(self, kwargs) -> BrokerClass:
+        broker_value = self.app.config.get("BROKER", "RABBITMQ")
+
+        broker_class = brokers.get(broker_value.upper(), None)
+        if broker_class is None:
+            raise ValueError(f"{broker_value} is not a valid broker")
+
+        broker = broker_class(self.app, kwargs)
+        return broker
+
+    @property
+    def broker(self) -> BrokerClass:
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, "broker"):
+                ctx.broker = self.connect(self.args)
+            return ctx.broker
+
+    def teardown(self, exception) -> None:
+        ctx = _app_ctx_stack.top
+        if hasattr(ctx, "broker"):
+            ctx.broker.close()
