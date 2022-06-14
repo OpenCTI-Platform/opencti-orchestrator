c83fbdd91215d2f3a84dd0321958c6fc467c0c1c
nor3th <73965465+nor3th@users.noreply.github.com>
2022-06-14 17:36:19 +0200

    [orchestrator] Fix tests and improve search query

--- app/core/heartbeat_service.py
+++ app/core/heartbeat_service.py
@@ -1,8 +1,4 @@
 import time
-from datetime import datetime
-
-# import Datetime as Datetime
-from flask import current_app
 from app.core.models import Connector, ConnectorInstance
 from app.extensions import scheduler
 
@@ -13,17 +9,26 @@ UNAVAILABLE = "unavailable"
 
 def heartbeat_service(interval: int):
     with scheduler.app.app_context():
+        scheduler.app.logger.debug("Heartbeat running")
+
         for connector in Connector.search().query("exists", field="uuid").execute():
-            for instance in (
-                ConnectorInstance.search()
-                .filter("term", connector_id=connector.meta.id)
-                .query("exists", field="last_seen")
-                .execute()
-            ):
+            scheduler.app.logger.debug(
+                f"Running connector {connector.name} ({connector.meta.id})"
+            )
+
+            results = ConnectorInstance.get_all(filters=[{"connector_id": connector.meta.id}])
+
+            for instance in results:
+                scheduler.app.logger.debug(
+                    f"Instance {instance.meta.id} {instance.status}"
+                )
                 if instance.last_seen > (time.time() - interval):
                     status = AVAILABLE
                 else:
                     status = UNAVAILABLE
 
                 if instance.status != status:
+                    scheduler.app.logger.info(
+                        f"Hearbeat change: {connector.name} ({connector.meta.id} instance {instance.meta.id} to {status}"
+                    )
                     instance.update(status=status)
--- tests/integration/test_heartbeat.py
+++ tests/integration/test_heartbeat.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import time
 
 from tests.utils.connector_creators import create_connector_and_config_stix
@@ -6,6 +7,7 @@ from pycti.connector.v2.libs.orchestrator_schemas import Instance
 
 
 def test_heartbeat(test_client, caplog):
+    caplog.set_level(logging.DEBUG, logger="app")
     config_id, connector_id, instance_id = create_connector_and_config_stix(test_client)
 
     # Check if available
@@ -14,8 +16,22 @@ def test_heartbeat(test_client, caplog):
     print(response.data.decode())
     instance = Instance(**json.loads(response.data.decode()))
     assert instance.status == "available"
-    # Just sleep longer than a heartbeat interval
-    time.sleep(10 + 3)  # 5 = heartbeat delay and 1 to avoid race condition
+    # Sleep longer than a heartbeat interval
+    time.sleep(10 + 3)  # 10 = heartbeat delay and 3 to avoid race condition
+
+    message = None
+    for record in caplog.records:
+        if record.name != "app":
+            continue
+        if (
+            "Hearbeat change" in record.message
+            and instance.connector_id in record.message
+            and instance.id in record.message
+        ):
+            message = record.message
+            break
+
+    assert message is not None, "Heartbeat wasn't updated"
 
     # Check if unavailable
     response = test_client.get(f"/heartbeat/{instance_id}")
@@ -32,3 +48,7 @@ def test_heartbeat(test_client, caplog):
     assert response.status_code == 200
     instance = Instance(**json.loads(response.data.decode()))
     assert instance.status == "available"
+
+    test_client.delete(f"/connector/{instance_id}")
+    test_client.delete(f"/connector/{config_id}")
+    test_client.delete(f"/connector/{connector_id}")
